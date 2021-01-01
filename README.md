43d3bc906a5fceb1c2da6fecf62719df76bbe192
Priyanshul Govil <58387911+priyanshul-govil@users.noreply.github.com>
2021-01-01 22:05:26 +0530

    Update script.py and README.md

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # SIU Result Scraper
-Scrape [SIU result page](https://www.examination.siu.edu.in/examination/result.html) for results of your entire batch. All you need is the list of **PRNs** for which you want to find the result, and a range of possible **Seat Numbers**. The script will then do its magic, and store all results in a directory `./result` in the form of `PNG` images. Also, the script isn't pure magic, so wait for SIU to release the result before you start cursing the develepor.
+Scrape [SIU result page](https://www.examination.siu.edu.in/examination/result.html) for results of your entire batch. All you need is the list of **PRNs** for which you want to find the result, and a range of possible **Seat Numbers**. The script will then do its magic, and store all results in a directory `./result` in the form of `PNG` images. However, the script isn't pure magic, so wait for SIU to release the result before you start cursing the develepor.
 
 
 ## DISCLAIMER
@@ -27,11 +27,11 @@ Clone this repository to your local machine. Navigate to the root directory.
 
 Inside `script.py`, there are two places at the start of the file marked `# MAKE CHANGES HERE`. Go ahead and make the following changes:
     
-1. Change the first place such that the list `prn` contains all the required PRNs. It is the responsibility of the user to exclude PRNs which do not exist. Failure to do so will result in the program crashing unexpectedly.
+1. Change values of `start_prn` and `end_prn`. The script will work to capture results of this range of PRNs.
     
-2. Change the second place such that the list `seat_num` contains the seat numbers for each required PRN. Take a good logical guess and choose a wide range of seat numbers. It won't matter as long as its reasonable (or if you've got a supercomputer!).
+2. Change values of `start_seat_num` and `end_seat_num` such that the range `[start_seat_num, end_seat_num]` contains the seat numbers for each required PRN. Take a good logical guess and choose a wide range of seat numbers. It won't matter as long as its reasonable (or if you've got a supercomputer!).
 
-3. Run it now! What are you even waiting for?
+That's it. Run it now! What are you even waiting for?
 
 
 
--- script.py
+++ script.py
@@ -3,23 +3,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.keys import Keys
 
 from PIL import Image
+
 
 import os
 os.mkdir("result")
 
+
+# MAKE CHANGE HERE
+start_prn = 19020475001
+end_prn = 19020475025
+
+# MAKE CHANGE HERE
+start_seat_num = 100501
+end_seat_num = 100530
+
+
 prn = []
 seat_num = []
 
-# MAKE CHANGE HERE
-for i in range(19020475001, 19020475026):
+
+for i in range(start_prn, end_prn + 1):
     prn.append(i)
 
-# MAKE CHANGE HERE
-for i in range(100501, 100530):
+for i in range(start_seat_num, end_seat_num + 1):
     seat_num.append(i)
 
 
@@ -39,25 +47,42 @@ for username in prn:
     browser.switch_to.frame(0)
 
 
+    usernameExists = True
+    seatnumExists = False
+
+
     for password in seat_num:
 
         browser.find_element_by_id("login").send_keys(username)
 
         browser.find_element_by_name("Submit").click()
 
+        if "Login failed. Please try again." in browser.page_source:
+            usernameExists = false
+            break
+
         browser.find_element_by_id("login").send_keys(password)
 
         browser.find_element_by_name("Submit").click()
 
         try:
-            WebDriverWait(browser, 2).until(EC.alert_is_present())
+            WebDriverWait(browser, 3).until(EC.alert_is_present())
             alert = browser.switch_to.alert
             alert.accept()
 
         except TimeoutException:
+            seatnumExists = True
             break
+    
 
-    ele = browser.find_element("xpath", '/html')
+    if not usernameExists:
+        continue
+
+    if not seatnumExists:
+        continue
+
+
+    ele = browser.find_element("xpath", "/html")
     total_height = ele.size["height"] + 1000
 
     browser.set_window_size(1920, total_height)
@@ -70,12 +95,13 @@ for username in prn:
 
     x = location['x']
     y = location['y']
-    width = location['x']+size['width']
-    height = location['y']+size['height']
+    width = location['x'] + size['width']
+    height = location['y'] + size['height']
 
-    im = Image.open(f'./result/{username}.png')
+    im = Image.open(f"./result/{username}.png")
     im = im.crop((int(x), int(y), int(width), int(height)))
-    im.save(f'./result/{username}.png')
+    im.save(f"./result/{username}.png")
+
 
     seat_num.remove(password)
 
